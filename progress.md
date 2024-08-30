2611ea10b48948ac59e79512d4acf57f6d648976
Khatthaphone <8247636+khatthaphone@users.noreply.github.com>
2024-08-30 15:27:47 +0700

    Vector3

---
+++ progress.md
@@ -0,0 +1,4 @@
+Link:
+[https://drive.google.com/drive/u/0/folders/1CFIyVSe2WokXxDlax6-HGgjK9hvAmN5x](https://drive.google.com/drive/u/0/folders/1CFIyVSe2WokXxDlax6-HGgjK9hvAmN5x)
+
+Current Progress: 26. Vector3
--- src/main.ts
+++ src/main.ts
@@ -1,44 +1,57 @@
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/Addons.js'
+import * as THREE from "three"
+import { OrbitControls } from "three/examples/jsm/Addons.js"
 
 const scene = new THREE.Scene()
 
 const cubeGerometry = new THREE.BoxGeometry(1, 1, 1)
-const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0x3aef2f })
 
 const cubeMesh = new THREE.Mesh(cubeGerometry, cubeMaterial)
-cubeMesh.position.z = 1
-cubeMesh.position.x = 1
-cubeMesh.position.y = 1
+// cubeMesh.position.z = 1
+// cubeMesh.position.x = 1
+// cubeMesh.position.y = 1
+cubeMesh.rotateX(1)
 
 scene.add(cubeMesh)
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 30)
-// const camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0.1, 30)
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  30
+)
+// const aspectRatio = window.innerWidth / window.innerHeight
+// const camera = new THREE.OrthographicCamera(
+//   -1 * aspectRatio,
+//   1 * aspectRatio,
+//   1,
+//   -1,
+//   0.1,
+//   30
+// )
 
 camera.position.z = 5
 
-const canvas = document.querySelector('canvas.threejs') as HTMLCanvasElement
+const canvas = document.querySelector("canvas.threejs") as HTMLCanvasElement
 
 const renderer = new THREE.WebGLRenderer({ canvas, antialias: true })
 
 const controls = new OrbitControls(camera, renderer.domElement)
-// controls.enableDamping = true
-// controls.autoRotate = true
+controls.enableDamping = true
+controls.autoRotate = true
 
 renderer.setSize(window.innerWidth, window.innerHeight)
 renderer.render(scene, camera)
 
-console.log('device pixel ratio:', window.devicePixelRatio)
+console.log("device pixel ratio:", window.devicePixelRatio)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 const axesHelper = new THREE.AxesHelper(2)
 scene.add(axesHelper)
 
 const renderLoop = () => {
-
   // cubeGerometry.scale(1.0001, 1.0001, 1.0001)
-
+  8
   controls.update()
   renderer.render(scene, camera)
   window.requestAnimationFrame(renderLoop)
@@ -46,10 +59,21 @@ const renderLoop = () => {
 
 renderLoop()
 
-window.addEventListener('resize', () => {
+window.addEventListener("resize", () => {
+  if (camera instanceof THREE.OrthographicCamera) {
+    const aspectRatio = window.innerWidth / window.innerHeight
+    camera.left = -1 * aspectRatio
+    camera.right = -1 * aspectRatio
+  }
+
+  if (camera instanceof THREE.PerspectiveCamera) {
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    camera.aspect = window.innerWidth / window.innerHeight
+  }
+
   renderer.setSize(window.innerWidth, window.innerHeight)
-  camera.aspect = window.innerWidth / window.innerHeight
   camera.updateProjectionMatrix()
+  renderer.render(scene, camera)
 })
 
 // function animate() {
